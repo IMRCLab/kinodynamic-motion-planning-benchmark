75c96ed98d68ce964c2308446b8db69f24dc6861
Quim Ortiz - Work <quimortiz21@gmail.com>
2022-02-03 13:29:56 +0100

    first examples of car working

--- src/car_with_trailer.g
+++ src/car_with_trailer.g
@@ -10,27 +10,30 @@ pivot_wheel(robot){
 Q:<t(.2 0 0)>
 }
 
-front_wheel(pivot_wheel) {joint:hingeZ, q:[-.5], shape:ssBox,size:[0.2 0.05 .2  0.01], color:[1 0 0 .5 ], contact}
+front_wheel(pivot_wheel) {joint:hingeZ, q:[.0], shape:ssBox,size:[0.2 0.05 .2  0.01], color:[1 0 0 .5 ], contact}
 
 
 wheel_joint(front_wheel){shape:marker, size:[.2]}
 
+arm_pivot(robot) {Q:<t(0 0 0) d(90 0 0 1)>, shape:marker, size:[.3]}
+
+arm(arm_pivot) {joint:hingeZ, q:[1.8], shape:marker, size:[.2]}
+
+arm_(arm) { Q:<t(.1 0 0)> , shape:ssBox, size:[.2 .05 0.05 0.01] color:[.3 .3 .3 .5] }
+
+trailer(arm_) { Q:<t(.2 0 0)> , shape:ssBox, size:[.2 .2 0.05 0.01] color:[.3 .3 .3 .5] }
 
 
-arm(robot) {joint:hingeZ, q:[.3], shape:marker, size:[.2]}
-
-arm_(arm) { Q:<t(-.1 0 0)> , shape:ssBox, size:[.2 .05 0.05 0.01] }
-
-trailer(arm_) { Q:<t(-.2 0 0)> , shape:ssBox, size:[.2 .2 0.05 0.01] }
-
-
-pivot_arm2(arm){ Q:<t(-.35 0 0)> }
+pivot_arm2(arm){ Q:<t(.35 0 0)> }
 
 arm2(pivot_arm2) {
 # joint:hingeZ, 
 # q:[.3], 
 shape:marker, size:[.2]}
 
+goal_forward{ shape:marker, size:[.3], X:<t(2 .2 0) d(0 0 0 1)> }
+
+goal_curve{ shape:marker, size:[.3], X:<t(2 1 0) d(45 0 0 1)> }
 
 
 # shape:ssBox,size:[0.1 0.1 .2  0.01], color:[0 0 0 .5], contact}
--- src/trailer.cpp
+++ src/trailer.cpp
@@ -1,11 +1,14 @@
 
 #include "KOMO/komo.h"
 #include "Kin/F_qFeatures.h"
+#include "car_utils.hpp"
 #include <Kin/kin.h>
 #include <cassert>
 
-const double L = .2;
+const double L = .2;   // distance  rear-front wheels
+const double d1 = .35; // distance between car centers
 
+#if 0
 // feature
 struct CarDynamics : Feature {
   // xdot = s cos ( theta )
@@ -67,18 +70,15 @@ struct CarDynamics : Feature {
     return 3;
   }
 };
+#endif
 
 struct Trailer : Feature {
+  // See Quim's Drawing:
+  // theta1DOT - s / d1 * cos (theta) = 0
   void phi2(arr &y, arr &J, const FrameL &F) {
-    // equations are:
-    // book says:
-    // DOTtheta_1 = s / d1 * sin (theta_0 - theta_1)
-    // But in our implementation, theta1 is a relative angle.
-    // theta_0 + OurTheta_1 = theta_1 (we should check symbol in drawing)
-    // DOTtheta_0 + DOTOurTheta_1 = DOTtheta_1
-    // DOTtheta_0 + DOTOurTheta_1  - s / d1 * sin ( - OurTheta1 )  = 0
 
     CHECK_EQ(F.nd, 2, "need two frames");
+    std::cout << "ho" << std::endl;
 
     CHECK_EQ(F(0, 0)->joint->type, rai::JT_transXYPhi, "");
     CHECK_EQ(F(0, 1)->joint->type, rai::JT_hingeZ, "");
@@ -98,10 +98,108 @@ struct Trailer : Feature {
     arr t, Jt;
     arr tdot, Jtdot;
     F_qItself().setOrder(0).eval(t, Jt, FrameL{F(1, 1)}.reshape(1, -1));
-    F_qItself().setOrder(1).eval(tdot, Jtdot,
-                                 FrameL{F(1, 0), F(1, 1)}.reshape(-1, 1));
+    F_qItself().setOrder(1).eval(
+        tdot, Jtdot, FrameL{F(0, 1), F(1, 1)}.reshape(-1, 1)); // break here!!
+
+    // get First car speed
+    arr vel, Jvel;
+    FrameL Fvel = {F(0, 0), F(1, 0)}; // First column
+    get_speed(vel, Jvel, Fvel.reshape(-1, 1));
+
+    double ct = std::cos(t(0));
+    double st = std::sin(t(0));
+    y(0) = tdot(0) - vel(0) / d1 * ct;
+
+    std::cout << "y out " << y.N << y << std::endl;
+    if (!!J) {
+      // ROWS = 1 equations ; COLUMNS= 1 t +  1 tdot + 1 vel
+      arr Jl;
+      Jl.resize(1, 3);
+      Jl(0, 0) = vel(0) / d1 * st;
+      Jl(0, 1) = 1;
+      Jl(0, 2) = -1 / d1 * ct;
+
+      arr block_, block;
+      block_.setBlockMatrix(Jt, Jtdot);
+      block.setBlockMatrix(block_, Jvel);
+      J = Jl * block;
+      std::cout << "returning Jac " << std::endl;
+      std::cout << J << std::endl;
+    }
+  }
+
+  uint dim_phi2(const FrameL &F) {
+    (void)F;
+    std::cout << "dim is " << 1 << std::endl;
+    return 1;
+  }
+};
+
+struct FirstCarRotation : Feature {
+
+  void phi2(arr &y, arr &J, const FrameL &F) {
+
+    CHECK_EQ(F.nd, 2, "need two frames");
+
+    CHECK_EQ(F(0, 0)->joint->type, rai::JT_transXYPhi, "");
+    CHECK_EQ(F(0, 1)->joint->type, rai::JT_hingeZ, "");
+    CHECK_EQ(F(1, 0)->joint->type, rai::JT_transXYPhi, "");
+    CHECK_EQ(F(1, 1)->joint->type, rai::JT_hingeZ, "");
+
+    bool verbose = false;
+    if (verbose) {
+      std::cout << "frames " << std::endl;
+      std::cout << F.nd << std::endl;
+      std::cout << F(0, 0)->name << std::endl; // robot
+      std::cout << F(0, 1)->name << std::endl; // front wheel
+      for (auto &f : F) {
+        std::cout << f->name << std::endl;
+      }
+
+      std::cout << "**** " << std::endl;
+    }
+
+    // robot
+    arr r, Jr;
+    arr rdot, Jrdot;
+    F_qItself().setOrder(0).eval(r, Jr, FrameL{F(1, 0)}.reshape(1, -1));
+    F_qItself().setOrder(1).eval(rdot, Jrdot,
+                                 FrameL{F(0, 0), F(1, 0)}.reshape(-1, 1));
+
+    // phi
+    arr phi;
+    arr Jphi;
+    F_qItself().setOrder(0).eval(phi, Jphi, F(1, {1, 1}).reshape(1, -1));
+
+    std::cout << "r " << r << std::endl;
+    std::cout << "Jr " << Jr << std::endl;
+    std::cout << "rdot " << rdot << std::endl;
+    std::cout << "Jdot " << Jrdot << std::endl;
+    std::cout << "phi " << phi << std::endl;
+    std::cout << "Jphi " << Jphi << std::endl;
+
+    arr vel, Jvel;
+    FrameL Fvel = {F(0, 0), F(1, 0)}; // First column
+    get_speed(vel, Jvel, Fvel.reshape(-1, 1));
+
+    y.resize(1);
+    double tphi = std::tan(phi(0));
+    double cphi = std::cos(phi(0));
+    y(0) = rdot(2) - vel(0) / L * tphi;
 
     if (!!J) {
+      // ROWS = 1 equations ; COLUMNS= 3 rdot + 1 vel + 1 phi
+      arr Jl;
+      Jl.resize(1, 5);
+      Jl(0, 2) = 1;             // w.r.t rdot(2)
+      Jl(0, 3) = -1 / L * tphi; //
+      Jl(0, 4) = -vel(0) / (L * cphi * cphi);
+
+      arr block_, block;
+      block_.setBlockMatrix(Jrdot, Jvel);
+      block.setBlockMatrix(block_, Jphi);
+
+      J = Jl * block;
     }
   }
 
@@ -119,22 +217,55 @@ int main() {
   rai::Configuration C;
   C.addFile(filename);
 
-  auto car_name = "robot";
-  auto car_wheel_name = "front_wheel";
-  auto second_car = "arm";
-
   KOMO komo;
   komo.setModel(C, true);
 
-  komo.setTiming(1, 10, 5, 1);
-  komo.addObjective({}, make_shared<CarDynamics>(), {car_name, car_wheel_name},
-                    OT_eq, {1e1}, {0}, 1);
+  komo.setTiming(1, 20, 5, 2);
+  // komo.add_qControlObjective({},2,.1);
+  komo.add_qControlObjective({}, 2, 1);
+  komo.add_qControlObjective({}, 1, 1);
 
-  komo.addObjective({}, make_shared<Trailer>(), {car_name, second_car}, OT_eq,
+  // Position First Car
+  // V cos(theta) - xdot = 0
+  // V sin(theta) - ydot = 0
+  komo.addObjective({}, make_shared<UnicycleDynamics>(), {"robot"}, OT_eq,
                     {1e1}, {0}, 1);
 
-  komo.reportProblem();
-  komo.run_prepare(.1);
+  // Rotation First Car
+  // theta_dot = s / L * tan(phi)
+  komo.addObjective({}, make_shared<FirstCarRotation>(),
+                    {"robot", "front_wheel"}, OT_eq, {1e1}, {0}, 1);
 
-  komo.checkGradients(); // it breaks here, not giving the jacobian
+  // Rotation Trailer
+  komo.addObjective({}, make_shared<Trailer>(), {"robot", "arm"}, OT_eq, {1e1},
+                    {0}, 1);
+
+  // komo.addObjective({1., 1.}, FS_poseDiff, {"robot", "goal_forward"}, OT_eq,
+  //                   {1e2}); SEEMS OK
+
+  komo.addObjective({1., 1.}, FS_poseDiff, {"robot", "goal_curve"}, OT_eq,
+                    {1e2});
+
+  komo.run_prepare(0);
+  komo.reportProblem();
+  // komo.checkGradients(); // it breaks here, not giving the jacobian
+  //
+  //
+
+  // komo.initWithWaypoints( {
+
+  // goal_forward{ shape:marker, size:[.3], X:<t(2 .2 0) d(0 0 0 1)> }
+
+  komo.run();
+
+  komo.reportProblem();
+
+  komo.view(true);
+  komo.view_play(true);
+
+  komo.plotTrajectory();
+
+  do {
+    cout << '\n' << "Press a key to continue...";
+  } while (std::cin.get() != '\n');
 }
