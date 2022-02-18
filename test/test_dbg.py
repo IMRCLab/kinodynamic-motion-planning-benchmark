d34fb7053261a1e7d76d267ce854804d94fb315c
Wolfgang Hoenig <hoenig@tu-berlin.de>
2022-02-18 08:55:48 +0100

    update trailer test case

--- test/test_dbg.py
+++ test/test_dbg.py
@@ -5,7 +5,7 @@ import subprocess as sp
 
 def test_dbg1():
     case = "dbg/1"
-    cmd = "./main_rai -model \"../test/{0}/env.g\" -waypoints \"../test/{0}/result_ompl.yaml\" -N -1 -display 1 -animate 1 -order 1 -robot unicycle_first_order_2 -cfg \"../test/{0}/rai.cfg\" -env \"../benchmark/unicycle_first_order_2/wall_0.yaml\" -out \"../test/{0}/result_opt.yaml\"".format(case)
+    cmd = "./main_rai -model \"../test/{0}/env.g\" -waypoints \"../test/car_first_order_with_1_trailers_0/parallelpark_0_sst.yaml\" -N -1 -display 1 -animate 1 -order 1 -robot car_first_order_with_1_trailers_0 -cfg \"../test/{0}/rai.cfg\" -env \"../benchmark/car_first_order_with_1_trailers_0/parallelpark_0.yaml\" -out \"tmp.yaml\"".format(case)
     print("Running", cmd)
     out = sp.run(cmd.split())
     print("Running DONE", cmd)
