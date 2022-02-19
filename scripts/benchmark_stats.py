69aa5cfefd5b885590f0617400d6d1f21b0c8782
Wolfgang Hoenig <whoenig@caltech.edu>
2022-02-19 18:40:21 +0100

    improved visualization

--- scripts/benchmark_stats.py
+++ scripts/benchmark_stats.py
@@ -29,22 +29,23 @@ def main():
 		# "dbAstar-scp",
 	]
 
-	report = plot_stats.Report(results_path / "stats.pdf")
+	report = plot_stats.Report(results_path / "stats.pdf", T=5*60, dt=0.1)
 
 	for instance in instances:
-		report.start_experiment("{}".format(instance), 5*60, 0.1)
 		for alg in algs:
 			result_folder = results_path / instance / alg
 			stat_files = [str(p) for p in result_folder.glob("**/stats.yaml")]
 			# stat_files = [str(p) for p in result_folder.glob("000/stats.yaml")]
-			print(stat_files)
 			if len(stat_files) > 0:
-				report.load_stat_files(alg, stat_files)
-		report.add_time_cost_plot()
-		report.add_initial_time_cost_plot()
-		report.add_success_rate_plot()
-		report.add_boxplot_initial_time_plot()
-		report.add_boxplot_initial_cost_plot()
+				report.load_stat_files(instance, alg, stat_files)
+
+	report.add_barplot_initial_cost_plot(instances)
+	for instance in instances:
+		report.add_time_cost_plot(instance)
+		report.add_initial_time_cost_plot(instance)
+		report.add_success_rate_plot(instance)
+		report.add_boxplot_initial_time_plot(instance)
+		report.add_boxplot_initial_cost_plot([instance])
 
 	report.close()
 
