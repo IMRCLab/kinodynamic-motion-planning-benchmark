c16662d1a379a3428adabd4c563a9186ac434ea6
quim@thread-ripper <quimortiz21@gmail.com>
2023-06-09 12:14:36 +0200

    work on server

--- src/main_primitives.cpp
+++ src/main_primitives.cpp
@@ -10,7 +10,8 @@ enum class PRIMITIVE_MODE {
   stats = 6,
   cut = 7,
   shuffle = 8,
-  generate_rand = 9
+  generate_rand = 9,
+  bintoyaml = 10,
 };
 
 #include "generate_primitives.hpp"
@@ -81,8 +82,33 @@ int main(int argc, const char *argv[]) {
     generate_primitives_random(options_primitives, trajectories);
 
     trajectories.save_file_boost(out_file.c_str());
-    trajectories.save_file_yaml((out_file +  ".1000.yaml").c_str(), 1000);
+    trajectories.save_file_yaml((out_file + ".1000.yaml").c_str(), 1000);
     trajectories.compute_stats("tmp_stats.yaml");
+  }
+
+  if (mode == PRIMITIVE_MODE::bintoyaml) {
+
+    Trajectories trajectories;
+    trajectories.load_file_boost(in_file.c_str());
+
+    // Should I partition?
+
+    size_t max_primitives_per_file = 20000;
+    size_t num_files = trajectories.data.size() / max_primitives_per_file;
+    if (trajectories.data.size() % max_primitives_per_file != 0) {
+      num_files += 1;
+    }
+
+    for (size_t i = 0; i < num_files; i++) {
+
+      Trajectories tt;
+      size_t start_index = i * max_primitives_per_file;
+      size_t end_index =
+          std::min((i + 1) * max_primitives_per_file, trajectories.data.size());
+      tt.data = {trajectories.data.begin() + start_index,
+                 trajectories.data.begin() + end_index};
+      tt.save_file_yaml((in_file + ".p" + std::to_string(i) + ".yaml").c_str());
+    }
   }
 
   if (mode == PRIMITIVE_MODE::generate) {
@@ -116,7 +142,8 @@ int main(int argc, const char *argv[]) {
     //     static_cast<int>(SOLVER::traj_opt_free_time_linear);
 
     improve_motion_primitives(options_trajopt, trajectories,
-                              options_primitives.dynamics, trajectories_out, options_primitives);
+                              options_primitives.dynamics, trajectories_out,
+                              options_primitives);
 
     trajectories_out.save_file_boost(out_file.c_str());
     trajectories_out.save_file_yaml((out_file + ".yaml").c_str(), 1000);
